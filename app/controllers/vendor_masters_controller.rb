8e35936f4e760c8a7e44b49a2ad0b40e6ebed805
Jayesh Binwal <binwaljayesh@gmail.com>
2024-02-07 12:48:48 +0530

    .

--- app/controllers/vendor_masters_controller.rb
+++ app/controllers/vendor_masters_controller.rb
@@ -1,13 +1,37 @@
 class VendorMastersController < ApplicationController
-  before_action :set_vendor_master
+  before_action :set_vendor_master, only: [:show]
+
+  def index
+    @vendor_masters = VendorMaster.all
+  end
+
+  def new 
+    @vendor_master = VendorMaster.new
+  end
+
+  def create
+    @vendor_master = VendorMaster.new(vendor_master_params)
+    if @vendor_master.save
+      redirect_to new_vendor_master_business_partner_path(@vendor_master,@business_partner)
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def show
     @business_partners = @vendor_master.business_partners
   end
+
+
 
   private
 
   def set_vendor_master
     @vendor_master = VendorMaster.find(params[:id])
   end
+
+  def vendor_master_params
+    params.require(:vendor_master).permit(:customer_code, :customer_name, :corporate_number, :invoice_number)
 end
+end
+
